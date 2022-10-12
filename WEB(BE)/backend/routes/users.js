5e9b8af10691d0652d2c9c0ccf20663bc4129d75
1Park <39184453+1Park@users.noreply.github.com>
2022-10-12 12:10:11 +0000

    sqlite to json file

--- WEB(BE)/backend/routes/users.js
+++ WEB(BE)/backend/routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../users.json');
+
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+  var sqlToJson = require('../../sqlite_to_json');
+  sqlToJson.getUsers();
+  console.log("sended to front");
+  var data = require('../../users.json');
   res.send(data);
 });
 
